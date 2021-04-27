34263dbc3f80e5f3107fc6db7eb71361041033bf
forward0606 <chen910606@gmail.com>
2021-04-27 12:29:00 +0800

    main.c update
    main.c finished
    next: make data to test time

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include "mergeSort.h"
 #include "quickSort.h"
 #include "heapSort.h"
+#include<string.h>
+#define MaxSize 1000000
+#define MaxLine 105
 
 void helpmessage(void){
 	printf("---help message\n");
@@ -13,32 +16,76 @@ void helpmessage(void){
 	return;
 }
 
+int inputInt(int **A){
+	*A = (int *)malloc(MaxSize * sizeof(int));
+	int n = 0;
+	while(scanf("%d",*A+n)!=EOF){n++;}
+	return n;
+}
+
+int inputStr(char ***A){
+	*A = (char **)malloc(MaxSize * sizeof(char *));
+	int n = 0;
+	char line[MaxLine];
+	while(fgets(line,MaxLine,stdin)){
+		(*A)[n] = strdup(line);
+		n++;
+	}
+	return n;
+}
+
+void outputInt(int A[],int n){
+	for(int i = 0;i<n;i++){
+		printf("%d\n",A[i]);
+	}
+}
+
+void outputStr(char *A[],int n){
+	for(int i = 0;i<n;i++){
+		printf("%s",A[i]);
+	}
+}
+
 int main(int argc, char *argv[]){
 	if(argc != 3){
 		helpmessage();
 	}else{
 		if(strcmp(argv[2], "heapSort") == 0){
 			if(strcmp(argv[1], "-i") == 0){
-				//UNFINISHED
-				inputInt();
-				heapSortInt();
+				int *A ;
+				int n = inputInt(&A);
+				heapSortInt(A,n);
+				outputInt(A,n);
 			}else{
-
+				char **A;
+				int n = inputStr(&A);
+				heapSortStr(A,n);
+				outputStr(A,n);
 			}
 		}else if(strcmp(argv[2], "quickSort") == 0){
 			if(strcmp(argv[1], "-i") == 0){
-
+				int *A ;
+				int n = inputInt(&A);
+				quickSortInt(A,n);
+				outputInt(A,n);
 			}else{
-
+				char **A;
+				int n = inputStr(&A);
+				quickSortStr(A,n);
+				outputStr(A,n);
 			}
-
 		}else{
 			if(strcmp(argv[1], "-i") == 0){
-
+				int *A ;
+				int n = inputInt(&A);
+				mergeSortInt(A,n);
+				outputInt(A,n);
 			}else{
-
+				char **A;
+				int n = inputStr(&A);
+				mergeSortStr(A,n);
+				outputStr(A,n);
 			}
-			
 		}
 	}	
 	return 0;
